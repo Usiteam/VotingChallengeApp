861c7855fdaf6721ca5c93cc9a72a8076e987268
dilan <hiradilan@gmail.com>
2017-01-16 12:03:29 -0600

    Changed to white background, dark green squares

--- manage.py
+++ manage.py
@@ -8,10 +8,10 @@ manager = Manager(app)
 @manager.command
 def initdb():
     db.create_all()
-    #db.session.add(User(firstName="LeBron", lastName="James",email="dilan@webitup.com", password="test", stocks=[Tickers(ticker='ATVI', startingPrice=28.12)]))
+    db.session.add(User(firstName="LeBron", lastName="James",email="dilan@webitup.com", password="test", stocks=[Tickers(ticker='ATVI', startingPrice=28.12)]))
     apple = Tickers(ticker="AAPL", startingPrice=89.8)
     signet = Tickers(ticker="SIG", startingPrice=90)
-    #db.session.add(User(firstName="Dilan", lastName="Hira",email="dilan@utexas.edu", password="test", stocks=[apple, signet]))
+    db.session.add(User(firstName="Dilan", lastName="Hira",email="dilan@utexas.edu", password="test", stocks=[apple, signet]))
     db.session.add(User(firstName="Arnav", lastName="Jain",email="arnav@utexas.edu", password="test", stocks=[apple, signet]))
     #db.session.add(apple)
     db.session.commit()
