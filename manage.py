8bfb7cac4d4a926a26dd33250ccac0a8b7aded74
Arnav Jain <archiejain1021@gmail.com>
2017-01-18 08:36:21 -0600

    Fixed styling on exited positions

--- manage.py
+++ manage.py
@@ -11,8 +11,9 @@ def initdb():
     db.session.add(User(firstName="LeBron", lastName="James",email="dilan@webitup.com", password="test", stocks=[Tickers(ticker='ATVI', startingPrice=28.12)]))
     apple = Tickers(ticker="AAPL", startingPrice=89.8)
     signet = Tickers(ticker="SIG", startingPrice=90)
+    google = Tickers(ticker="GOOG", startingPrice=400)
     db.session.add(User(firstName="Dilan", lastName="Hira",email="dilan@utexas.edu", password="test", stocks=[apple, signet]))
-    db.session.add(User(firstName="Arnav", lastName="Jain",email="arnav@utexas.edu", password="test", stocks=[apple, signet]))
+    db.session.add(User(firstName="Arnav", lastName="Jain",email="arnav@utexas.edu", password="test", stocks=[google, apple, signet]))
     #db.session.add(apple)
     db.session.commit()
     print 'Initialized the database'
