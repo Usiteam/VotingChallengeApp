7ca5c0dcaa80d0f524d0f8b426052c0578db6994
Arnav Jain <archiejain1021@gmail.com>
2017-06-30 22:16:25 -0500

    Testing out change

--- manage.py
+++ manage.py
@@ -6,21 +6,21 @@ import requests
 
 manager = Manager(app)
 
-@manager.command
-def initdb():
-    db.create_all()
-    apple = Tickers(ticker="AAPL", startingPrice=89.8, short=True)
-    signet = Tickers(ticker="SIG", startingPrice=90)
-    google = Tickers(ticker="GOOG", startingPrice=400)
-    aal = Tickers(ticker="AAL", startingPrice=40.00)
-    tesla = Tickers(ticker="TSLA", startingPrice=50.00)
-    admin = Role(name="Admin", description="Gets to look at all the rankings.")
-    db.session.add(User(firstName="Dilan", lastName="Hira",email="dilan@utexas.edu", password="test", stocks=[apple, signet]))
-    db.session.add(User(firstName="Arnav", lastName="Jain",email="arnav@utexas.edu", password="test", stocks=[google, apple, signet, aal, tesla], roles=[admin]))
-    #db.session.add(apple)
-    db.session.commit()
-    refreshdb()
-    print 'Initialized the database'
+# @manager.command
+# def initdb():
+#     db.create_all()
+#     apple = Tickers(ticker="AAPL", startingPrice=89.8, short=True)
+#     signet = Tickers(ticker="SIG", startingPrice=90)
+#     google = Tickers(ticker="GOOG", startingPrice=400)
+#     aal = Tickers(ticker="AAL", startingPrice=40.00)
+#     tesla = Tickers(ticker="TSLA", startingPrice=50.00)
+#     admin = Role(name="Admin", description="Gets to look at all the rankings.")
+#     db.session.add(User(firstName="Dilan", lastName="Hira",email="dilan@utexas.edu", password="test", stocks=[apple, signet]))
+#     db.session.add(User(firstName="Arnav", lastName="Jain",email="arnav@utexas.edu", password="test", stocks=[google, apple, signet, aal, tesla], roles=[admin]))
+#     #db.session.add(apple)
+#     db.session.commit()
+#     refreshdb()
+#     print 'Initialized the database'
 
 @manager.command
 def dropdb():
