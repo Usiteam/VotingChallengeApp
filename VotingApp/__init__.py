1b8c9e877645f91802cb07b6121362cfb9a2b518
Arnav Jain <archiejain1021@gmail.com>
2017-09-06 16:05:05 -0500

    Changed the email account to send from

--- VotingApp/__init__.py
+++ VotingApp/__init__.py
@@ -9,7 +9,7 @@ from cryptography.fernet import Fernet
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
-app = Flask(__name__)
+app = Flask(__name__)	
 app.config['SECRET_KEY'] = '~t\x86\xc9\x1ew\x8bOcX\x85O\xb6\xa2\x11kL\xd1\xce\x7f\x14<y\x9e'
 # app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'thermos.db')
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
@@ -37,8 +37,8 @@ app.config.update(
 	MAIL_SERVER='smtp.gmail.com',
 	MAIL_PORT=465,
 	MAIL_USE_SSL=True,
-	MAIL_USERNAME = 'technology@usiteam.org',
-	MAIL_PASSWORD = 'Arjo2323@#'
+	MAIL_USERNAME = 'votingchallenge@usiteam.org',
+	MAIL_PASSWORD = 'votingchallenge2017'
 	)
 
 mail=Mail(app)
