fd4ca4a768e503140b06d52788812d1e1235ec80
Arnav Jain <archiejain1021@gmail.com>
2017-10-13 20:50:34 -0500

    turn off ssl

--- VotingApp/mainApp.py
+++ VotingApp/mainApp.py
@@ -21,6 +21,17 @@ from openpyxl import load_workbook, Workbook
 from sqlalchemy import func
 from grampg import PasswordGenerator
 import ntpath
+import ssl
+
+try:
+    _create_unverified_https_context = ssl._create_unverified_context
+except AttributeError:
+    # Legacy Python that doesn't verify HTTPS certificates by default
+    pass
+else:
+    # Handle target environment that doesn't support HTTPS verification
+    ssl._create_default_https_context = _create_unverified_https_context
+
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
